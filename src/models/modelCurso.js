d651d6344bdb2e25afec73bbc1b8749f22549262
Aparecida <aparecidalml0@gmail.com>
2026-05-10 21:25:55 -0300

    armazenamento em arquivo

--- src/models/modelCurso.js
+++ src/models/modelCurso.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const caminho = path.join(__dirname, 'database', 'cursos.json')
+const caminho =  path.join(__dirname, '..', 'database', 'cursos.json')
 
 export function lerCursos() {
     try {
